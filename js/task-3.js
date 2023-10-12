04ef0769327a1aae79b67096521107cca2cbce7c
YuliiaaLevchenko <141173167+YuliiaaLevchenko@users.noreply.github.com>
2023-10-13 01:08:00 +0300

    hw js 01

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+"use strict"
+
+function getElementWidth(content, padding, border) {
+let contentNumber = Number.parseFloat(content);
+let paddingNumber = Number.parseFloat(padding);
+let borderNumber = Number.parseFloat(border);
+const totalElementWidth = contentNumber + paddingNumber * 2 + borderNumber * 2;
+return totalElementWidth;
+}
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
